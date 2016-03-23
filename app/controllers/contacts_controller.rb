f71a9d42c7f869ce7634f66395eadfb6c2b9549e
nagaraju12 <kammaranagaraju@gmail.com>
2016-03-23 17:20:58 +0530

    some more changes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,22 @@
 class ContactsController < ApplicationController
   before_filter :is_login?,only: [:index]
 layout :layout?, only: [:index,:show,:edit,:destroy]
-def index
-  @contacts = Contact.all
+
+  def index
+     @contacts = Contact.all
   
-    if params[:search]
+        if params[:search]
        @contacts = Contact.search(params[:search]).order("created_at DESC")
     else
        @contacts = Contact.all.order('created_at DESC')
     end
 end
-		
 
-	def new
+def new
 		@contact= Contact.new
 			@cart= current_cart
 	end
-
-	def create
+def create
 		@contact= Contact.new(contact_params)
 		UserMailer.contact_email(@contact).deliver
 		if @contact.save
@@ -26,16 +25,15 @@ end
 			render "new"
 		end
 	end
-
-	def show
+def show
 		@contact= Contact.find(params[:id])
 	end
 
-	def edit
+def edit
 		@contact= Contact.find(params[:id])
 	end
 
-	def update
+def update
 		@contact= Contact.find(params[:id])
 		if @contact.update(contact_params)
 			redirect_to contacts_path
@@ -43,19 +41,16 @@ end
 			render "edit"
 		end
 	end
-
-	def destroy
+def destroy
 		@contact= Contact.find(params[:id])
 		@contact.destroy
 		redirect_to contacts_path
 	end
 
-	private
+private
 
-	def contact_params
+def contact_params
 		params.require(:contact).permit!
 	end
-
-
 
 end
